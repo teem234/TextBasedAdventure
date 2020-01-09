02889284e0c273b141295a44ae0c5c98b5269967
Tim Jay <tim.joyce33@gmail.com>
2020-01-09 12:47:59 -0500

    Updated README and gitignore to include debug folder

---
+++ debug/CMakeFiles/mygame.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/td/CLionProjects/mygame/CombatUnit.cpp" "/home/td/CLionProjects/mygame/debug/CMakeFiles/mygame.dir/CombatUnit.cpp.o"
+  "/home/td/CLionProjects/mygame/Enemy.cpp" "/home/td/CLionProjects/mygame/debug/CMakeFiles/mygame.dir/Enemy.cpp.o"
+  "/home/td/CLionProjects/mygame/GameController.cpp" "/home/td/CLionProjects/mygame/debug/CMakeFiles/mygame.dir/GameController.cpp.o"
+  "/home/td/CLionProjects/mygame/Item.cpp" "/home/td/CLionProjects/mygame/debug/CMakeFiles/mygame.dir/Item.cpp.o"
+  "/home/td/CLionProjects/mygame/ItemDamage.cpp" "/home/td/CLionProjects/mygame/debug/CMakeFiles/mygame.dir/ItemDamage.cpp.o"
+  "/home/td/CLionProjects/mygame/ItemHealth.cpp" "/home/td/CLionProjects/mygame/debug/CMakeFiles/mygame.dir/ItemHealth.cpp.o"
+  "/home/td/CLionProjects/mygame/Level.cpp" "/home/td/CLionProjects/mygame/debug/CMakeFiles/mygame.dir/Level.cpp.o"
+  "/home/td/CLionProjects/mygame/LevelBoss.cpp" "/home/td/CLionProjects/mygame/debug/CMakeFiles/mygame.dir/LevelBoss.cpp.o"
+  "/home/td/CLionProjects/mygame/LevelChest.cpp" "/home/td/CLionProjects/mygame/debug/CMakeFiles/mygame.dir/LevelChest.cpp.o"
+  "/home/td/CLionProjects/mygame/LevelEmpty.cpp" "/home/td/CLionProjects/mygame/debug/CMakeFiles/mygame.dir/LevelEmpty.cpp.o"
+  "/home/td/CLionProjects/mygame/LevelFight.cpp" "/home/td/CLionProjects/mygame/debug/CMakeFiles/mygame.dir/LevelFight.cpp.o"
+  "/home/td/CLionProjects/mygame/LevelNode.cpp" "/home/td/CLionProjects/mygame/debug/CMakeFiles/mygame.dir/LevelNode.cpp.o"
+  "/home/td/CLionProjects/mygame/LevelShop.cpp" "/home/td/CLionProjects/mygame/debug/CMakeFiles/mygame.dir/LevelShop.cpp.o"
+  "/home/td/CLionProjects/mygame/Player.cpp" "/home/td/CLionProjects/mygame/debug/CMakeFiles/mygame.dir/Player.cpp.o"
+  "/home/td/CLionProjects/mygame/main.cpp" "/home/td/CLionProjects/mygame/debug/CMakeFiles/mygame.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ debug/CMakeFiles/mygame.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mygame.dir/main.cpp.o"
+  "CMakeFiles/mygame.dir/Enemy.cpp.o"
+  "CMakeFiles/mygame.dir/Item.cpp.o"
+  "CMakeFiles/mygame.dir/LevelNode.cpp.o"
+  "CMakeFiles/mygame.dir/GameController.cpp.o"
+  "CMakeFiles/mygame.dir/CombatUnit.cpp.o"
+  "CMakeFiles/mygame.dir/Player.cpp.o"
+  "CMakeFiles/mygame.dir/LevelChest.cpp.o"
+  "CMakeFiles/mygame.dir/LevelFight.cpp.o"
+  "CMakeFiles/mygame.dir/Level.cpp.o"
+  "CMakeFiles/mygame.dir/ItemHealth.cpp.o"
+  "CMakeFiles/mygame.dir/ItemDamage.cpp.o"
+  "CMakeFiles/mygame.dir/LevelBoss.cpp.o"
+  "CMakeFiles/mygame.dir/LevelEmpty.cpp.o"
+  "CMakeFiles/mygame.dir/LevelShop.cpp.o"
+  "mygame.pdb"
+  "mygame"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mygame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
